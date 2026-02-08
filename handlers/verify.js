4f342b5949cb4b8d87e173b4fafd22fdc385d531
Clickdev <clickshop1987@gmail.com>
2026-02-08 16:30:55 +0530

    Update verify.js

--- handlers/verify.js
+++ handlers/verify.js
@@ -1,8 +1,18 @@
-const { showWelcome, verifiedUsers } = require("./user");
+const isJoined = require("../utils/channelCheck");
+const { verifiedUsers, showWelcome } = require("./user");
 
 exports.verify = async (bot, query) => {
   const userId = query.from.id;
   const chatId = query.message.chat.id;
+
+  // 🔴 CHECK CHANNEL JOIN
+  const joined = await isJoined(bot, userId);
+  if (!joined) {
+    return bot.answerCallbackQuery(query.id, {
+      text: "❌ Please join official & order alert channels first!",
+      show_alert: true
+    });
+  }
 
   verifiedUsers.add(userId);
 
