153ee1ea7a52e7489fc48530d15927f1b5cd74b1
Clickdev <clickshop1987@gmail.com>
2026-02-08 16:20:34 +0530

    Update verify.js

--- handlers/verify.js
+++ handlers/verify.js
@@ -1,24 +1,8 @@
-const isJoined = require("../utils/channelCheck");
 const { showWelcome, verifiedUsers } = require("./user");
-
-// MUST JOIN CHANNELS
-const REQUIRED_CHANNELS = [
-  "@OrdersNotify",
-  "@SheinVoucherHub"
-];
 
 exports.verify = async (bot, query) => {
   const userId = query.from.id;
   const chatId = query.message.chat.id;
-
-  const joined = await isJoined(bot, userId, REQUIRED_CHANNELS);
-
-  if (!joined) {
-    return bot.answerCallbackQuery(query.id, {
-      text: "❌ Please join all mandatory channels first!",
-      show_alert: true
-    });
-  }
 
   verifiedUsers.add(userId);
 
@@ -26,6 +10,5 @@ exports.verify = async (bot, query) => {
     text: "✅ Verified successfully!"
   });
 
-  // First time welcome
   showWelcome(bot, chatId, userId);
 };
